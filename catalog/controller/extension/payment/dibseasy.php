6274e9fee55863c0465668f794ae83322bcd48e4
maxwhite <maxwhite1983@gmail.com>
2019-02-12 11:32:48 +0200

    Fixes #1

--- catalog/controller/extension/payment/dibseasy.php
+++ catalog/controller/extension/payment/dibseasy.php
@@ -20,8 +20,6 @@ class ControllerExtensionPaymentDibseasy extends Controller {
                         
                        if(isset($response->payment->paymentDetails->paymentType) && $response->payment->paymentDetails->paymentType) {
                             
-                            $maskedCardNumber = $response->payment->paymentDetails->cardDetails->maskedPan;
-                            $cardPostfix = substr($maskedCardNumber, -4);
                             $this->session->data['dibseasy_transaction'] = $_GET['paymentId'];
                             $res = $this->model_extension_payment_dibseasy->getCountryByIsoCode3($response->payment->consumer->shippingAddress->country);
                             $country = $res['name'];
@@ -60,9 +58,14 @@ class ControllerExtensionPaymentDibseasy extends Controller {
                             }
                             $transactDetails = "$transactionId: <b>{$response->payment->paymentId}</b> <br>"
                                              . "$paymentType:   <b>{$response->payment->paymentDetails->paymentType}</b> <br>"
-                                             . "$paymentMethod: <b>{$response->payment->paymentDetails->paymentMethod}</b> <br>"
-                                             . "$cardNumberPostfix: <b>$cardPostfix</b>";
+                                             . "$paymentMethod: <b>{$response->payment->paymentDetails->paymentMethod}</b> <br>";
                             
+                             if(isset($response->payment->paymentDetails->cardDetails->maskedPan)) {
+                                 $maskedCardNumber = $response->payment->paymentDetails->cardDetails->maskedPan;
++                                $cardPostfix = substr($maskedCardNumber, -4);
++                                $transactDetails .= "$cardNumberPostfix: <b>$cardPostfix</b>";
+                             }            
+                                             
                             $this->load->model('checkout/order');
                             $this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('dibseasy_order_status_id'), $transactDetails, true);
                             
