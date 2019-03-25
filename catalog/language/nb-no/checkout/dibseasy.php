112fe3db4b2c98ea8625215e1c1464d7db1646b2
maxwhite <maxwhite1983@gmail.com>
2019-03-25 18:15:29 +0200

    added norwegian translation

---
+++ catalog/language/nb-no/checkout/dibseasy.php
@@ -0,0 +1,6 @@
+<?php
+
+$_['button_checkout_label'] = 'Bruk annen fraktmetode';
+$_['order_summary_label'] = 'Ordresammendrag';
+$_['shipping_methods_label'] = 'Fraktmetoder';
+$_['shipping_total_label'] = 'Frakt';
